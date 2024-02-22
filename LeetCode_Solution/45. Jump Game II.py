1e1a42b58860e0a306773af20c51fd3282567598
Jibek Gupta <jibek.gupta@bison.howard.edu>
2024-02-22 02:28:13 -0500

    #45 Array DynamicProgramming Greedy

---
+++ LeetCode_Solution/45. Jump Game II.py
@@ -0,0 +1,32 @@
+'''You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+0 <= j <= nums[i] and
+i + j < n
+Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can reach nums[n - 1].
+Example 1:
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: nums = [2,3,0,1,4]
+Output: 2'''
+
+def jump(nums):
+    res=0
+    l,r=0,0
+
+    while r< len(nums)-1:
+        far=0
+        for i in range(l,r+1):
+            far=max(far,i+nums[i])
+        l=r+1
+        r=far
+        res+=1
+    return res
+
+
+nums1=[2,3,1,1,4]
+nums2=[2,3,0,1,4]
+print(jump(nums1))
+print(jump(nums2))
